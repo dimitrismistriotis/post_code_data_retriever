6853232097e5cbdc20bda654ffe6f6035c8a4001
Dimitrios Mistriotis <dimitrismistriotis@gmail.com>
2013-08-02 15:45:54 +0100

    Test for illegal post code.

--- spec/post_code_data_retriever_spec.rb
+++ spec/post_code_data_retriever_spec.rb
@@ -2,5 +2,11 @@ require "spec_helper"
 require 'post_code_data_retriever'
 
 describe PostCodeDataRetriever do
-
+  context 'filter illegal post code' do
+    it 'should return an error on illegal post code' do
+      process_result = subject.get_coordinates('dd')
+      expect(process_result.result_code).to equal(-1)
+      expect(process_result.error).to equal('Input provided is not a post code.')
+    end
+  end
 end
