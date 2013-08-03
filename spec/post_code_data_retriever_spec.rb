352d6f1b7d7ebe152ff7c494b2d27e7e45197981
Dimitrios Mistriotis <dimitrismistriotis@gmail.com>
2013-08-03 16:38:09 +0100

    Specs for correct message to http resource.

--- spec/post_code_data_retriever_spec.rb
+++ spec/post_code_data_retriever_spec.rb
@@ -11,22 +11,24 @@ describe PostCodeDataRetriever do
   end
 
   context 'retrieve data from uk-postcodes on valid post code' do
-    #    .http://uk-postcodes.com/postcode/SE165DY.json
-    it 'should issue an http request on legal post code' do
+    before do
       results = IO.read("#{File.dirname(__FILE__)}/data/uk-postcodes_response-se165dy.json")
       HTTParty.
         should_receive(:get).
-        with('http://uk-postcodes.com/postcode/SE165DY.json',)
-        .and_return(results)
+        with('http://uk-postcodes.com/postcode/SE165DY.json')
+        .and_return({ code: 200, body: results })
+    end
+
+    it 'should issue an http request on legal post code' do
       process_result = subject.get_coordinates('SE16 5DY')
       expect(process_result.result_code).to equal(0)
       expect(process_result.error).not_to eq('Input provided is not a valid UK post code.')
     end
 
-    #it 'should issue an http request on legal post code with custom format' do
-    #  process_result = subject.get_coordinates('se165dy')
-    #  expect(process_result.result_code).to equal(0)
-    #  expect(process_result.error).not_to eq('Input provided is not a valid UK post code.')
-    #end
+    it 'should issue an http request on legal not formatted post code' do
+      process_result = subject.get_coordinates('se165dy')
+      expect(process_result.result_code).to equal(0)
+      expect(process_result.error).not_to eq('Input provided is not a valid UK post code.')
+    end
   end
 end
