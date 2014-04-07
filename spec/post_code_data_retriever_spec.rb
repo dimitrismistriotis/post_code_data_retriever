26c265184534ade6d4f425930fbade3a0f34cd68
Dimitrios Mistriotis <dimitrismistriotis@gmail.com>
2014-04-07 08:06:59 +0100

    Refactored existing tests to use VCR.

--- spec/post_code_data_retriever_spec.rb
+++ spec/post_code_data_retriever_spec.rb
@@ -2,7 +2,7 @@ require 'spec_helper'
 require 'post_code_data_retriever'
 
 describe PostCodeDataRetriever do
-  context 'filter illegal post code' do
+  context 'should filter illegal post code' do
     it 'should return an error on illegal post code' do
       process_result = subject.get_coordinates('dd')
       expect(process_result.result_code).to equal(-1)
@@ -11,62 +11,46 @@ describe PostCodeDataRetriever do
   end
 
   context 'Data retrieval and parsing for existing post code' do
-    before do
-
-      mocked_response = Object.new
-      def mocked_response.code
-        return 200
-      end
-      def mocked_response.body
-        return IO.read("#{File.dirname(__FILE__)}/data/uk-postcodes_response-se165dy.json")
-      end
-
-      HTTParty
-        .should_receive(:get)
-        .with('http://uk-postcodes.com/postcode/SE165DY.json')
-        .and_return(mocked_response)
-    end
-
     context 'retrieve data from uk-postcodes on valid post code' do
       it 'should issue an http request on legal post code' do
-        process_result = subject.get_coordinates('SE16 5DY')
-        expect(process_result.result_code).to equal(0)
-        expect(process_result.error).not_to eq('Input provided is not a valid UK post code.')
+        VCR.use_cassette('se165dy_response') do
+          process_result = subject.get_coordinates('SE16 5DY')
+          expect(process_result.result_code).to equal(0)
+          expect(process_result.error).not_to eq(
+            'Input provided is not a valid UK post code.')
+        end
       end
 
       it 'should issue an http request on legal not formatted post code' do
-        process_result = subject.get_coordinates('se165dy')
-        expect(process_result.result_code).to equal(0)
-        expect(process_result.error).not_to eq('Input provided is not a valid UK post code.')
+        VCR.use_cassette('se165dy_response') do
+          process_result = subject.get_coordinates('se165dy')
+          expect(process_result.result_code).to equal(0)
+          expect(process_result.error).not_to eq(
+            'Input provided is not a valid UK post code.')
+        end
       end
     end
 
     context 'Mapping retrieved result' do
       it 'map returned result' do
-        process_result = subject.get_coordinates('SE16 5DY')
-        expect(process_result.result_code).to equal(0)
-        expect(process_result.latitude.to_s).to eq('51.505821')
+        VCR.use_cassette('se165dy_response') do
+          process_result = subject.get_coordinates('SE16 5DY')
+          expect(process_result.result_code).to equal(0)
+          expect(process_result.latitude.to_s).to eq('51.505822348111835')
+        end
       end
     end
   end
 
   context 'Response for valid but non-exitant post code' do
+
     # http://uk-postcodes.com/postcode/SE165DG.json
-    before do
-
-      mocked_response = Object.new
-      def mocked_response.code() 404 end
-      def mocked_response.body() '' end
-
-      HTTParty
-        .should_receive(:get)
-        .with('http://uk-postcodes.com/postcode/SE165DG.json')
-        .and_return(mocked_response)
-    end
-
-    it 'return error with non existent flag' do
-      process_result = subject.get_coordinates('SE16 5DG')
-      expect(process_result.result_code).to equal(-4)
+    #
+    it 'should return error with non existent flag' do
+      VCR.use_cassette('se165dg_response') do
+        process_result = subject.get_coordinates('SE16 5DG')
+        expect(process_result.result_code).to equal(-4)
+      end
     end
   end
 
